f0003c7075bbb9d6caa22b199c8db9ce34173de4
felipe.aravena.a <felipe.aravena.a@gmail.com>
2024-05-18 15:23:32 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'social-fipo-app',
+  webDir: 'www'
+};
+
+export default config;
